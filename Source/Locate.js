b0d4cf3e41bf22d16f456438c825b456ba5f49b5
Christopher Beloch <webmaster@cbeloch.de>
2010-02-21 00:16:18 +0100

    added 'watcher' function. support for repeated position updates

--- Source/Locate.js
+++ Source/Locate.js
@@ -19,7 +19,8 @@ provides: [Locate]
 var Locate = new Class({
 	Implements: [Options, Events],
 	options: {
-		loi: true // loi = locate on init
+		loi: true, // loi = locate on init
+		loiType: 'locate' // locate OR watch
 	},
 	
 	initialize: function(options){
@@ -27,23 +28,53 @@ var Locate = new Class({
 		
 		thisBind = this; // Need some bind variable for later, don't know it better
 		
-		if(this.options.loi)
-			this.locate();
+		if(this.options.loi){
+			switch(this.options.loiType){
+				case 'locate':
+					this.locate();
+					break;
+				case 'watch':
+					this.watcher();
+					break;
+				default:
+					this.fireEvent("error", "loiType unknown");
+			}
+		}
+	},
+	
+	setPosition: function(position){
+		thisBind.position = {
+			lat: position.coords.latitude,
+			long: position.coords.longitude
+		};
+	
+		thisBind.fireEvent("locate", thisBind.position);		
+	},
+	handleError: function(error){
+		this.fireEvent("error", error.message)
 	},
 	
 	locate: function(){
 		if (navigator.geolocation)    
-			navigator.geolocation.getCurrentPosition(this.setPosition);
+			navigator.geolocation.getCurrentPosition(this.setPosition, this.handleError);
 		else
-			this.fireEvent("error");
+			this.fireEvent("error", "geolocation is not supported");
 	},
 	
-	setPosition: function(position){
-		this.position = {
-			lat: position.coords.latitude,
-			long: position.coords.longitude
-		};
+	watcher: function(){
+		// have to call it 'watcher'
+		// 'watch' is causing errors on Firefox, MobileSafari works perfect
+		if (navigator.geolocation)    
+			this.watchId = navigator.geolocation.watchPosition(this.setPosition, this.handleError);
+		else
+			this.fireEvent("error", "geolocation is not supported");
+	},
+	
+	stopWatcher: function(){
+		if (navigator.geolocation)    
+			this.watchId = navigator.geolocation.clearWatch(this.watchId);
+		else
+			this.fireEvent("error", "geolocation is not supported");
 		
-		thisBind.fireEvent("locate", this.position);		
 	}
 });
