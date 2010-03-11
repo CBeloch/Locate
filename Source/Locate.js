96ed746265c42de3156c8339da66e49d38e35400
Christopher Beloch <webmaster@cbeloch.de>
2010-03-11 15:57:00 +0100

    added position.headingStr - NOT PART OF W3C SPECIFICATIONS!

--- Source/Locate.js
+++ Source/Locate.js
@@ -2,7 +2,7 @@
 ---
 
 script: Locate.js
-version: 1.1
+version: 1.2
 description: With the Locate class you can retrieve the current position of your visitors
 license: MIT-style
 authors:
@@ -53,10 +53,11 @@ var Locate = new Class({
 	
 	setPosition: function(position){
 		this.position = position.coords;
+		this.position.headingStr = this.headingString();
 		this.fireEvent("locate", this.position);		
 	},
 	handleError: function(error){
-		this.fireEvent("error", error.message)
+		this.fireEvent("error", "Error " + error.code + " - " + error.message)
 	},
 	
 	locate: function(){
@@ -71,5 +72,26 @@ var Locate = new Class({
 	
 	stopWatcher: function(){
 		this.watchId = navigator.geolocation.clearWatch(this.watchId);		
+	},
+	
+	headingString: function(){
+		if(!this.position.heading) // if heading is null
+			return null;
+		if(this.position.heading >= 337.5 || (this.position.heading >= 0 && this.position.heading <= 22.5))
+			return "N";
+		if(this.position.heading >= 22.5 || this.position.heading <= 67.5)
+			return "NE";
+		if(this.position.heading >= 67.5 || this.position.heading <= 112.5)
+			return "E";
+		if(this.position.heading >= 112.5 || this.position.heading <= 157.5)
+			return "SE";
+		if(this.position.heading >= 157.5 || this.position.heading <= 202.5)
+			return "S";
+		if(this.position.heading >= 202.5 || this.position.heading <= 247.5)
+			return "SW";
+		if(this.position.heading >= 247.5 || this.position.heading <= 292.5)
+			return "W";
+		if(this.position.heading >= 292.5 || this.position.heading <= 337.5)
+			return "NW";
 	}
 });
